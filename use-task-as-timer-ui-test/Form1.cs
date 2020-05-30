1f87acc5ff9e239d7ea872917b3218240d494605
IVSoftware <tgregor@ivsoftware.com>
2020-05-30 16:29:41 -0600

    Add project files.

---
+++ use-task-as-timer-ui-test/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace use_task_as_timer_ui_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            RestartDelay();
+        }
+        void RestartDelay()
+        {
+            Task.Delay(1000).GetAwaiter().OnCompleted(() => { PerformUpdate(); });
+        }
+
+
+        Stopwatch _stopwatch = new Stopwatch();
+        private void PerformUpdate()
+        {
+            Task.Run(() =>
+            {
+                Debug.WriteLine(_stopwatch.Elapsed.ToString() + " - Update Controls");
+                BeginInvoke((MethodInvoker)delegate
+                {
+                    richTextBox1.AppendText(
+                        Environment.NewLine + "...updating..." + Environment.NewLine
+                    );
+                });
+                Task.Delay(5000).Wait();
+            }).GetAwaiter().OnCompleted(()=>
+            {
+                RestartDelay();
+            });
+        }
+    }
+}
---
+++ use-task-as-timer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace use_task_as_timer
+{
+    class Program
+    {
+        static Startup _app;
+        static void Main(string[] args)
+        {
+            _app = new Startup();
+            Console.ReadKey(); // Block here for demo
+        }
+    }
+    public partial class Startup
+    {   
+
+        void RestartDelay()
+        {
+            Task.Delay(1000).GetAwaiter().OnCompleted(() => { PerformUpdate(); });
+        }
+
+        private void PerformUpdate()
+        {
+            Debug.WriteLine(_stopwatch.Elapsed.ToString() + " - Update Controls");
+            RestartDelay();
+        }
+        public Startup()
+        {
+            Configuration(new AppBuilder());
+        }
+        
+        Stopwatch _stopwatch = new Stopwatch();    
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            _stopwatch.Start();
+            RestartDelay();
+        }
+
+        private void ConfigureAuth(IAppBuilder app) { } // Pretend
+        public interface IAppBuilder { } // Pretend
+        public class AppBuilder : IAppBuilder { } // Pretend
+    }
+}
