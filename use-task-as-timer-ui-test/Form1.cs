e249978022c2a6ac19cc23cbf0f3e22e0536e821
IVSoftware <tgregor@ivsoftware.com>
2020-05-30 16:38:59 -0600

    Added a UI project to test the claim that we can avoid blocking UI thread.

--- use-task-as-timer-ui-test/Form1.cs
+++ use-task-as-timer-ui-test/Form1.cs
@@ -23,23 +23,38 @@ namespace use_task_as_timer_ui_test
             Task.Delay(1000).GetAwaiter().OnCompleted(() => { PerformUpdate(); });
         }
 
-
         Stopwatch _stopwatch = new Stopwatch();
         private void PerformUpdate()
         {
             Task.Run(() =>
             {
                 Debug.WriteLine(_stopwatch.Elapsed.ToString() + " - Update Controls");
-                BeginInvoke((MethodInvoker)delegate
-                {
-                    richTextBox1.AppendText(
-                        Environment.NewLine + "...updating..." + Environment.NewLine
-                    );
-                });
-                Task.Delay(5000).Wait();
+                LongRunningUpdateTask();
+
             }).GetAwaiter().OnCompleted(()=>
             {
                 RestartDelay();
+            });
+        }
+        private void LongRunningUpdateTask()
+        {
+            BeginInvoke((MethodInvoker)delegate
+            {
+                richTextBox1.AppendText(
+                    Environment.NewLine + "BeginUpdate..." + Environment.NewLine
+                );
+            });
+
+            // This is a stress test to make sure we're
+            // not blocking the UI thread even when we 
+            // block THIS thread for 5 seconds at a time.
+
+            Task.Delay(5000).Wait();
+            BeginInvoke((MethodInvoker)delegate
+            {
+                richTextBox1.AppendText(
+                    Environment.NewLine + "...EndUpdate" + Environment.NewLine
+                );
             });
         }
     }
